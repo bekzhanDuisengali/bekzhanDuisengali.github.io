4ee3e53e332190211c702dcaf72bba8d889fcb52
bekzhanduisengali <beckzhan04@gmail.com>
2025-04-10 23:26:14 +0500

    paswword settings

--- backend/db.js
+++ backend/db.js
@@ -4,7 +4,7 @@ const pool = new Pool({
     user: process.env.DB_USER || 'postgres',
     host: process.env.DB_HOST || 'localhost',
     database: process.env.DB_NAME || 'postgres',
-    password: process.env.DB_PASSWORD || 'твой_пароль',
+    password: process.env.DB_PASSWORD || 'bekzhan',
     port: process.env.DB_PORT || 5433,
 });
 
